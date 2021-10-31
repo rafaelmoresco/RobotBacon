519e9e285388d4243937f0371410aa4e210d24c6
Moresco <rafaelmorescovieira@gmail.com>
2021-10-31 11:00:35 -0300

    changed role

--- commands/leave.js
+++ commands/leave.js
@@ -6,7 +6,7 @@ module.exports = {
 		.setDescription('You lost NNN'),
 	async execute(interaction) {
 		try {
-			interaction.member.roles.remove('904366388917960735');
+			interaction.member.roles.remove('904007020980613172');
 			return interaction.reply('This user lost NNN');
 		} catch(err) {
 			return interaction.reply('You already lost');
